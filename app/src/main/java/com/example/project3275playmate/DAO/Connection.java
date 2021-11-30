75fc55be13b55ccef63e4e72f1672e026c32a03c
DavidW-12239 <davidw12239@gmail.com>
2021-11-30 00:31:51 -0800

    update

--- app/src/main/java/com/example/project3275playmate/DAO/Connection.java
+++ app/src/main/java/com/example/project3275playmate/DAO/Connection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class Connection {
     public java.sql.Connection connect() throws SQLException, ClassNotFoundException{
         Class.forName("com.mysql.cj.jdbc.Driver");
-        java.sql.Connection con1 = DriverManager.getConnection("jdbc:mysql://localhost/PlayMate", "root","");
+        java.sql.Connection con1 = DriverManager.getConnection("jdbc:mysql://localhost/playmate", "root","");
         return con1;
     }
 }
