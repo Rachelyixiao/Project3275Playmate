c7db5a78331096f18d951892be59e2ba20f1cd3d
DavidW-12239 <davidw12239@gmail.com>
2021-12-02 15:14:04 -0800

    update

--- app/src/main/java/com/example/project3275playmate/RegisterLogin/LoginPage.java
+++ app/src/main/java/com/example/project3275playmate/RegisterLogin/LoginPage.java
@@ -1,24 +1,55 @@
 package com.example.project3275playmate.RegisterLogin;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.project3275playmate.DAO.DAO;
 import com.example.project3275playmate.R;
 
+import java.sql.SQLException;
+
 public class LoginPage extends AppCompatActivity {
-    EditText emailLogin;
+    EditText nameLogin;
     EditText pwLogin;
     TextView resetPw;
     Button btnLoginSubmit;
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
+    String toast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginpage);
-        emailLogin = findViewById(R.id.nameLogin);
+        nameLogin = findViewById(R.id.nameLogin);
         pwLogin = findViewById(R.id.pwLogin);
         resetPw = findViewById(R.id.resetPw);
         btnLoginSubmit = findViewById(R.id.btnLoginSubmit);
+        sp = getSharedPreferences("user", Context.MODE_PRIVATE); //Save data to the next pages
+        editor = sp.edit();
+    }
+
+    public void login(View view){
+        DAO dao = new DAO(this);
+        String name = nameLogin.getText().toString().trim();
+        String password = pwLogin.getText().toString().trim();
+        try {
+            toast = dao.login(name, password);
+            editor.putString("name", name);
+            editor.commit();
+            Toast.makeText(this, toast, Toast.LENGTH_LONG).show();
+            if (!toast.contains("Register Successful!")){
+                return;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
