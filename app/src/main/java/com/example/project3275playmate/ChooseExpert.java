07f83c3ea70601fb99aabaa2284947a00ae86071
your name <your_email@youremail.com>
2021-12-02 19:04:31 -0800

    CustomerMainPage  ChooseExpert

--- app/src/main/java/com/example/project3275playmate/ChooseExpert.java
+++ app/src/main/java/com/example/project3275playmate/ChooseExpert.java
@@ -1,12 +1,14 @@
 package com.example.project3275playmate;
 
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.project3275playmate.Homepage.CustomerMainPage;
 import com.example.project3275playmate.R;
+import com.example.project3275playmate.RegisterLogin.LoginPage;
 
 public class ChooseExpert extends AppCompatActivity {
     Spinner userChooseGameList;
@@ -18,6 +20,7 @@ public class ChooseExpert extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_expert);
+        SharedPreferences Choice = getSharedPreferences("Choice",MODE_PRIVATE);
 
         //两种选择方式，选择游戏，显示该游戏所有玩家信息；选择性别，选择该性别所有的玩家
         userChooseGameList = findViewById(R.id.userChooseGameList);
@@ -26,8 +29,38 @@ public class ChooseExpert extends AppCompatActivity {
         userChooseBoy =findViewById(R.id.userChooseBoy);
         userChooseGameGo = findViewById(R.id.userChooseGameGo);
         userChooseGenderGo = findViewById(R.id.userChooseGenderGo);
+        SharedPreferences.Editor editor = Choice.edit();
+        userChooseGameGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String  currencyChoice = userChooseGameList.getSelectedItem().toString();
 
+                editor.putString("Game", currencyChoice);
+                editor.commit();
+                startActivity(new Intent(ChooseExpert.this, ExpertList.class));
+            }
+        });
 
+        userChooseGenderGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (userChooseGirl.isChecked()){
+                    editor.putString("Choice", "Girl");
+                    editor.commit();//选的是女生，把关键词Girl保存并进入下一页
+                    startActivity(new Intent(ChooseExpert.this, ExpertList.class));
+                }
+                else if (userChooseBoy.isChecked()){
+                    editor.putString("Choice", "Boy");
+                    editor.commit();//选的是男生，把关键词Boy保存并进入下一页
+                    startActivity(new Intent(ChooseExpert.this, ExpertList.class));
+                }
+                else{
+                    //未选性别，错误提示
+                    Toast.makeText(ChooseExpert.this,"Please select a Gender",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/project3275playmate/Homepage/CustomerMainPage.java
+++ app/src/main/java/com/example/project3275playmate/Homepage/CustomerMainPage.java
@@ -1,26 +1,53 @@
 package com.example.project3275playmate.Homepage;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.project3275playmate.AfterPlayPages.ReviewExpert;
 import com.example.project3275playmate.ChooseExpert;
 import com.example.project3275playmate.Classes.TopUp;
+import com.example.project3275playmate.DAO.DAO;
 import com.example.project3275playmate.R;
+
+import java.sql.SQLException;
 
 public class CustomerMainPage extends AppCompatActivity {
     Button btnChooseExpert,btnTopUp,btnReviewExpert,btnViewBalanceUse;
+    TextView username,balance;
+    SharedPreferences sp;
+    String name;
+    double Balance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_main_page);
-
+        sp = getSharedPreferences("user", Context.MODE_PRIVATE);
         btnChooseExpert = findViewById(R.id.btnChooseExpert);
         btnTopUp = findViewById(R.id.btnTopup);
         btnReviewExpert = findViewById(R.id.btnReviewExpert);
         btnViewBalanceUse = findViewById(R.id.btnViewBalanceUse);
+        username = findViewById(R.id.Username);
+        balance = findViewById(R.id.Balance);
+
+        DAO dao = new DAO(this);
+        name = sp.getString("name","");
+        username.setText(name);
+        try {
+            Balance = dao.searchCus(name).getBalance();
+            balance.setText(Double.toString(Balance));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+        }
+
+
 
         btnChooseExpert.setOnClickListener(new View.OnClickListener() {
             @Override
