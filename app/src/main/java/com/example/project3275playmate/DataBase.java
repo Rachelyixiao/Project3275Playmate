5383f0ba5a763204b8f74ef039feabebcc28623d
Rachelyixiao <angelinexiaoxiao@gmail.com>
2021-12-03 16:51:25 -0800

    update

--- app/src/main/java/com/example/project3275playmate/DataBase.java
+++ app/src/main/java/com/example/project3275playmate/DataBase.java
@@ -177,9 +177,11 @@ public class DataBase extends SQLiteOpenHelper {
 
 
         String query14 = "INSERT INTO "+TABLE6_NAME+" VALUES "+"("+
-                "'PUBG',"+"'Winnie',"+"4.5,"+"16.0,"+"250.00"+"),"+"("+
-                "'Steve',"+"'boy',"+"4.6,"+"18.0,"+"150.00"+"),"+"("+
-                "'Fiona',"+"'girl',"+"4.2,"+"15.0,"+"100.0"+")";
+                "'PUBG',"+"'Winnie',"+"'diamond4'"+"),"+"("+
+                "'Dota2',"+"'Winnie',"+"'4000dp'"+"),"+"("+
+                "'PUBG',"+"'Fiona',"+"'goldern'"+"),"+"("+
+                "'Dota2',"+"'Fiona',"+"'5000dp'"+")";
+        db.execSQL(query14);
     }
 
     @Override
